95c14057a5809b07003807458d053eccb919c24e
Quang Vo <thanhquang2608@gmail.com>
2019-05-28 01:56:49 +0700

    update

--- predict.js
+++ predict.js
@@ -37,15 +37,21 @@ const takePhoto = function ({ photo, message }, done) {
 }
 
 const takeScreenShot = async (html) => {
+  const width = 600;
+  const height = 518;
   const browser = await puppeteer.launch({
     args: [
-      `--window-size=${600},${518}`,
+      `--window-size=${width},${height}`,
       '--no-sandbox',
       '--disable-setuid-sandbox'
     ] // new option
   });
   const page = await browser.newPage();
   await page.setContent(html);
+  await page.setViewport({
+    width,
+    height
+  });
   const base64 = await page.screenshot({ encoding: "base64" });
   await browser.close();
   return base64; 
