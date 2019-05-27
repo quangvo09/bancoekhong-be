6fe38ae258880825baae83d5a739a03b625f513b
Quang Vo <thanhquang2608@gmail.com>
2019-05-27 23:22:55 +0700

    init server

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+const port = process.env.PORT || 3000
+
+app.set('port', port);
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', function(req, res) {
+  res.json('Server is running...');
+})
+
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+});
